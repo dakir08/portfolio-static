44646b8c052a61617881118109043dc34aba4bcb
Yuuki <minhphuongsony@gmail.com>
2020-07-06 19:32:57 +1000

    Updates

---
+++ precache-manifest.cc62a24099b176302551824cc46f8ad9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27e32defc5fe6f034af79965d8e0312e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5847e00bfde8efded3f5",
+    "url": "/static/css/2.e6f51e71.chunk.css"
+  },
+  {
+    "revision": "15ee3733ed0af5b42e92",
+    "url": "/static/css/main.16ff6fe4.chunk.css"
+  },
+  {
+    "revision": "5847e00bfde8efded3f5",
+    "url": "/static/js/2.4ae12879.chunk.js"
+  },
+  {
+    "revision": "9450e89efc00352ad8dfbbdbfb1ef77a",
+    "url": "/static/js/2.4ae12879.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15ee3733ed0af5b42e92",
+    "url": "/static/js/main.2472249e.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "ce2f150a350252f4efec5db02a262594",
+    "url": "/static/media/avatar.ce2f150a.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "6e46c5449b86e5bd5ab627281df78dd5",
+    "url": "/static/media/front-image.6e46c544.jpg"
+  },
+  {
+    "revision": "943328d10c64301f1cc09dfc85147491",
+    "url": "/static/media/main-bg.943328d1.jpg"
+  },
+  {
+    "revision": "2297bb1a3b1311ac6564fd870fed399c",
+    "url": "/static/media/pre-loading.2297bb1a.gif"
+  },
+  {
+    "revision": "421b78fb2a90f606b3e8123738560099",
+    "url": "/static/media/profile.421b78fb.png"
+  }
+]);
